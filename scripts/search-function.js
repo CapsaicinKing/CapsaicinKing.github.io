b8af18f7f3375a830fad4bdc4d8a90884f40303e
CapsaicinKing <113944859+CapsaicinKing@users.noreply.github.com>
2022-11-21 15:18:51 -0600

    fixed the searchProjects() syntax error

--- scripts/search-function.js
+++ scripts/search-function.js
@@ -1,7 +1,7 @@
 function searchProjects() {
     let query = document.getElementById("search-query").value.toLowerCase();
     let projectSlip = document.getElementsByClassName("project-slip");
-    let projectNames = document.getElementsByClassName("project-name");
+    let projectNames = document.getElementsByClassName("light-project-name");
     let hr = document.getElementsByTagName("hr");
     for (let i = 0; i < projectSlip.length; i++) {
         if (projectNames[i].innerHTML.toLowerCase().includes(query)) {
