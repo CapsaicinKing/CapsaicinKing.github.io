293c45d2bb0c25aa2ef0f828189fb31f76bacd39
CapsaicinKing <113944859+CapsaicinKing@users.noreply.github.com>
2022-11-25 12:10:17 -0600

    modal show and exit

---
+++ scripts/info-card.js
@@ -0,0 +1,10 @@
+function showModal() {
+  let modal = document.getElementById("info-modal-wrapper");
+  modal.style.display = "flex";
+}
+
+
+function exitModal() {
+  let modal = document.getElementById("info-modal-wrapper");
+  modal.style.display = "none";
+}
